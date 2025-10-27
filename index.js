797191a9128e075d4948660cebb750eca29b8e45
Md Muqtadir Fuad <mmfuad01@gmail.com>
2025-10-27 16:59:37 +0600

    index update

---
+++ index.js
@@ -0,0 +1,19 @@
+let myLeads = ["abc.com",'knasjnd',"jkbaskjdbjk"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    //console.log(myLeads)
+})
+
+for( let i = 0;i<myLeads.length; i++ ){
+    //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    //create element
+    //set text content
+    //append to ul
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
+}
