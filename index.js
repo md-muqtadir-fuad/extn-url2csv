bb1cc7289106d2f83a61fa270158807e42aa0cde
Md Muqtadir Fuad <mmfuad01@gmail.com>
2025-10-28 21:50:37 +0600

    clear input field added

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-let myLeads = ["abc.com",'knasjnd',"jkbaskjdbjk"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
-    //console.log(myLeads)
+    renderLeads();
+    clearInputField();
 })
 
-for( let i = 0;i<myLeads.length; i++ ){
-    //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    //create element
-    //set text content
-    //append to ul
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        listItems += "<li>" + myLeads[i] + "</li>"
+        //create element
+        //set text content
+        //append to ul
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
+    ulEl.innerHTML = listItems
 }
+
+function clearInputField() {
+    let inputField = document.getElementById("input-el");
+    inputField.value = "";
+
+}
+
